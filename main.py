b01f305c4996cff51ad1f34ffe813d79f4514455
MikkilineniNithish <215396780+MikkilineniNithish@users.noreply.github.com>
2026-06-19 14:53:04 +0530

    Add CORS support

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from farmer_agent import ask_farmer_agent
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
